439083ffd26a1f8c32f23d224303e9bc1d6836af
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-23 18:47:05 +0300

    Сделал 1 задание

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,54 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
+
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return ` <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </div>`;
+    })
+    .join("");
+}
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+galleryRef.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  const eventTarget = event.target;
+  const sourceUrl = eventTarget.dataset.source;
+  console.log(sourceUrl);
+  const instance = basicLightbox.create(
+    `
+    <img src="${sourceUrl}" width="800" height="600">
+`,
+    {
+      onShow: (instance) => {
+        instance.element().querySelector("img").onclick = instance.close;
+        function closeModalKey(event) {
+          if (event.code === "Escape") {
+            instance.close();
+            return;
+          }
+          window.addEventListener("keydown", closeModalKey);
+        }
+      },
+      onClose: (instance) => {},
+    }
+  );
+
+  instance.show();
+}
+
+// galleryRef.addEventListener("click", openModal);
