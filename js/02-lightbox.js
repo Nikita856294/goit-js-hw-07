53d6f28ed95fffa4f44361b03e7a617a5f51753a
Nikita Nikitenko <84923367+Nikita856294@users.noreply.github.com>
2021-10-24 17:42:32 +0300

    сделал 2 задание

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGallery(galleryItems);
 
-console.log(galleryItems);
+function createGallery(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return ` 
+      <li>
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="Image ${description}" />
+    </a>
+    </li>`;
+    })
+    .join("");
+}
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+lightbox.on("show.simplelightbox");
